9f1c65fb8abd7bc70760f742fc869600834be9b4
bradstw <brad@silvertapllc.com>
2019-10-29 15:05:10 -0400

    addRecordToList function working

--- src/Methods/Contacts.php
+++ src/Methods/Contacts.php
@@ -26,18 +26,54 @@ class Contacts implements MethodInterface
         $this->client = $connect->getClient();
     }
     
+    /**
+     * Add Record to a list
+     * @param array (list name, array(record to be added))
+     *
+     * @return array (info on request, # updated, added or failed)
+     */
     public function addRecordToList($listName, $record)
     {
+        $method = 'addRecordToList';
+        
+        # Build fields mapping and importdata objects
         $columnNumber = 1;
         $columns = [];
         $data = [];
-        foreach ($contactFields as $field => $value) {
+        foreach ($record as $field => $value) {
             $columns[] = [
                 "columnNumber" => $columnNumber++,
                 "fieldName"    => $field,
-                "key"          => ($field == 'number1' ? true : false)
+                "key"          => ($field == 'number1' ? true : false),
             ];
             array_push($data, $value);
+        }
+        # Build object and send record to list
+        #TODO create options in function for diff settings to be passed
+        $list_update_settings = [
+            'fieldsMapping' => $columns,
+            'reportEmail' => 'dialer@silvertapllc.com',
+            'skipHeaderLine' => false,
+            'crmAddMode' => 'ADD_NEW',
+            'crmUpdateMode' => 'UPDATE_FIRST',
+            'listAddMode' => 'ADD_FIRST',
+            'callNowMode' => 'ANY',
+            'cleanListBeforeUpdate' => false,
+        ];
+        $xml_data = [
+            'listName' => $listName,
+            'listUpdateSettings' => $list_update_settings,
+            'record' => $data,
+        ];
+        
+        try {
+            $result = $this->client->$method($xml_data);
+            $vars = get_object_vars($result);
+            $response = get_object_vars($vars['return']);
+            return $response;
+        } catch (\Exception $e) {
+            $error_message = $e->getMessage();
+            return $error_message;
         }
     }
 }
